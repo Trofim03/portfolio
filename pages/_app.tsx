44862e29ca46f3b1629ba4b336a6e811a7cdccf4
trofim.ivanchenko <trofim.ivanchenko@nordclan.com>
2023-01-23 13:48:28 +0300

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { MainLayout } from "../components/MainLayout";
 
-import Head from "next/head";
-
 import { motion } from "framer-motion";
 
 import "../i18n/i18n";
@@ -24,36 +22,6 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   };
 
   return (
-    <>
-      <Head>
-        <title>TI</title>
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
       <MainLayout>
         <motion.div
           key={router.route + language}
@@ -64,7 +32,6 @@ function MyApp({ Component, pageProps, router }: AppProps) {
           <Component {...pageProps} />
         </motion.div>
       </MainLayout>
-    </>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,55 @@
-import type { NextPage } from 'next'
-import { IndexAbout } from '../PagesFragments/index/About'
-import { Contacts } from '../PagesFragments/index/Contacts';
-import { Experience } from '../PagesFragments/index/Experience';
-import { MyLinks } from '../PagesFragments/index/MyLinks';
-import { Stack } from '../PagesFragments/index/Stack'
-import css from './index.module.scss';
+import type { NextPage } from "next";
+import { IndexAbout } from "../PagesFragments/index/About";
+import { Contacts } from "../PagesFragments/index/Contacts";
+import { Experience } from "../PagesFragments/index/Experience";
+import { MyLinks } from "../PagesFragments/index/MyLinks";
+import { Stack } from "../PagesFragments/index/Stack";
+import css from "./index.module.scss";
 
+
+import Head from "next/head";
 
 const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>TI</title>
 
-	return (
-		<div className={css.main}>
-			<IndexAbout />
-			<Stack />
-			<Experience />
-			<Contacts />
-			<MyLinks />
-		</div>
-	)
-}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <div className={css.main}>
+        <IndexAbout />
+        <Stack />
+        <Experience />
+        <Contacts />
+        <MyLinks />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
